5db21ae784748b50e245d64eb2738ace691c5bf1
ntk-batch12 <ahmad.noor24@outlook.com>
2024-09-17 18:40:40 -0400

    Update DemoDay3.java

--- src/day3/DemoDay3.java
+++ src/day3/DemoDay3.java
@@ -5,7 +5,8 @@ public class DemoDay3 {
 	public static void main(String[] args) {
 		
 		System.out.println("Day 3 Team 1");
-
+		System.out.println("abc");
+		System.out.println("xyz");
 		
 		int a = 10;
 		int b = 20; 
